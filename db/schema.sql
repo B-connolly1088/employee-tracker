f8b48eb12a14068bb3c59f7a721f31959d4b4107
Ben laptop <b.connolly1088@gmail.com>
2023-06-20 16:26:11 -0600

    finished table schemas

---
+++ db/schema.sql
@@ -0,0 +1,43 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE `department` (
+    `id` INT  NOT NULL ,
+    `name` VARCHAR(30)  NOT NULL ,
+    PRIMARY KEY (
+        `id`
+    )
+);
+
+CREATE TABLE `role` (
+    `id` INT  NOT NULL ,
+    `title` VARCHAR(30)  NOT NULL ,
+    `salary` DECIMAL  NOT NULL ,
+    `department_id` INT  NOT NULL ,
+    PRIMARY KEY (
+        `id`
+    )
+);
+
+CREATE TABLE `employee` (
+    `id` INT  NOT NULL ,
+    `first_name` VARCHAR(30)  NOT NULL ,
+    `last_name` VARCHAR(30)  NOT NULL ,
+    `role_id` INT  NOT NULL ,
+    `manager_id` INT  NOT NULL ,
+    PRIMARY KEY (
+        `id`
+    )
+);
+
+ALTER TABLE `role` ADD CONSTRAINT `fk_role_department_id` FOREIGN KEY(`department_id`)
+REFERENCES `department` (`id`);
+
+ALTER TABLE `employee` ADD CONSTRAINT `fk_employee_role_id` FOREIGN KEY(`role_id`)
+REFERENCES `role` (`id`);
+
+ALTER TABLE `employee` ADD CONSTRAINT `fk_employee_manager_id` FOREIGN KEY(`manager_id`)
+REFERENCES `employee` (`id`);
+
